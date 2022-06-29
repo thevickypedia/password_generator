3141c959338697bb874eb60903233a97f44b4779
thevickypedia <vignesh.sivanandarao@gmail.com>
2022-06-29 04:47:13 -0500

    Add `UNICODE_PREFIX` to make it harder to read

--- hexify.py
+++ hexify.py
@@ -1,32 +1,40 @@
-import os
-import random
+import base64
+import binascii
 import re
-from binascii import hexlify, unhexlify
+import string
 from itertools import zip_longest
 
-string = "This is a sample string to be secured"
+input_string = "This is a sample string to be secured"
+
+UNICODE_PREFIX = base64.b64decode(b'XA==').decode(encoding="ascii") + string.ascii_letters[20] + string.digits[:1] * 2
 
 
-def deep_hex():
+def deep_hex(hexed):
     return [
-        '\\u00' + '\\u00'.join(re.findall('..?', str_to_hex)),
-        '\\u00' + '\\u00'.join(map('{}{}'.format, *(str_to_hex[::2], str_to_hex[1::2]))),
-        '\\u00' + '\\u00'.join(a + b for a, b in zip(str_to_hex[::2], str_to_hex[1::2])),
-        '\\u00' + '\\u00'.join(str_to_hex[i:i + 2] for i in range(0, len(str_to_hex), 2)),
-        '\\u00' + '\\u00'.join(f'{str_to_hex[i:i + 2]}' for i in range(0, len(str_to_hex), 2)),
-        '\\u00' + '\\u00'.join([''.join(item) for item in zip(str_to_hex[::2], str_to_hex[1::2])]),
-        '\\u00' + '\\u00'.join(a + b for a, b in zip_longest(str_to_hex[::2], str_to_hex[1::2], fillvalue="")),
-        ''.join([f'\\u00{e}{str_to_hex[i + 1]}' for i, e in enumerate(str_to_hex) if i < len(str_to_hex) - 1][::2])
+        UNICODE_PREFIX + UNICODE_PREFIX.join(re.findall('..?', hexed)),
+        UNICODE_PREFIX + UNICODE_PREFIX.join(map('{}{}'.format, *(hexed[::2], hexed[1::2]))),
+        UNICODE_PREFIX + UNICODE_PREFIX.join(a + b for a, b in zip(hexed[::2], hexed[1::2])),
+        UNICODE_PREFIX + UNICODE_PREFIX.join(hexed[i:i + 2] for i in range(0, len(hexed), 2)),
+        UNICODE_PREFIX + UNICODE_PREFIX.join(f'{hexed[i:i + 2]}' for i in range(0, len(hexed), 2)),
+        UNICODE_PREFIX + UNICODE_PREFIX.join([''.join(item) for item in zip(hexed[::2], hexed[1::2])]),
+        UNICODE_PREFIX + UNICODE_PREFIX.join(a + b for a, b in zip_longest(hexed[::2], hexed[1::2],
+                                                                           fillvalue="")),
+        UNICODE_PREFIX + UNICODE_PREFIX.join([e + hexed[i + 1] for i, e in enumerate(hexed)
+                                              if i < len(hexed) - 1][::2]),
     ]
 
 
-str_to_hex = hexlify(string.encode(encoding='utf-8')).decode(encoding='utf-8')
-str_to_hex = random.choice(deep_hex()) if os.environ.get('DEEP_HEX') else str_to_hex
-if str_to_hex.startswith('\\'):
-    print("Processing as bytes")
-    hex_to_str = bytes(str_to_hex, "utf-8").decode(encoding="unicode_escape")
-else:
-    print("Processing as hex")
-    hex_to_str = unhexlify(str_to_hex).decode("utf-8")
+def hexlify():
+    str_to_hex = binascii.hexlify(input_string.encode(encoding='utf-8')).decode(encoding='utf-8')
+    for each in deep_hex(hexed=str_to_hex):
+        if each.startswith('\\'):
+            print("Processing as bytes")
+            hex_to_str = bytes(each, "utf-8").decode(encoding="unicode_escape")
+        else:
+            print("Processing as hex")
+            hex_to_str = binascii.unhexlify(each).decode("utf-8")
+        assert hex_to_str == input_string
 
-assert hex_to_str == string
+
+if __name__ == '__main__':
+    hexlify()
